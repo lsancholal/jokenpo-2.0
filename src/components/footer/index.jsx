758740353029db03960357a0661ff864ca80db28
sanchola <gmsanches190@gmail.com>
2024-08-14 15:10:44 -0300

    first commit

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,17 @@
+import styles from './footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+           <div>
+
+           
+
+           </div>
+            
+
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/header/index.jsx
@@ -0,0 +1,16 @@
+import styles from './header.module.css'
+
+function Header() {
+    return (
+        <header className={styles.header}>
+            <div>
+
+            <p>Pedra Papel e Tesoura</p>
+
+            </div>
+
+        </header>
+    )
+}
+
+export default Header
